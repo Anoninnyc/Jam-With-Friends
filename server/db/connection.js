287098e0d53010ea3bd1b4daa037c4a4d746bc74
Krishan Arya <krishanmarya1@gmail.com>
2017-03-13 21:56:14 -0800

    Reintorduce conn

---
+++ server/db/connection.js
@@ -0,0 +1,17 @@
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+const db_host = process.env.heroku_db_host;
+const db_user = process.env.heroku_db_user;
+const db_password = process.env.heroku_db_password;
+const db_name = process.env.heroku_db_name;
+
+const sequelize = new Sequelize(db_name, db_user, db_password, {
+  host: db_host,
+  dialect: 'postgres' /*'mysql'*/
+});
+
+module.exports = {
+  sequelize,
+  Sequelize
+};
